960fc455c8bcbed209fab96856ea453de4a632ee
yaremchuky <yaremchuky@OMSL0004.Chl-batt.local>
2014-04-01 15:19:50 +0100

    Session 1

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -10,8 +10,31 @@ Ext.application({
             html: 'Simple HTML panel',
             items: [{
             	xtype: 'toolbar',
-            	docked: 'top'
-            		
+            	title: 'Top menu',
+            	docked: 'top',
+            	items: [{
+            		xtype: 'button',
+                	text: 'Cancel'
+            	}, {
+            		xtype: 'spacer'
+            	}, {
+            		xtype: 'button',
+                	text: 'Save',
+                	handler: function() {
+                		Ext.Msg.alert('Title', 'You just pressed Save button', Ext.emptyFn);
+                	}
+            	}]
+            }, {
+            	xtype: 'list',
+            	itemTpl: '{title}',
+            	height: '100%',
+            	data: [{
+            		title: 'Patient 1'
+            	}, {
+            		title: 'Patient 2'
+            	}, {
+            		title: 'Patient 3'
+            	}]
             }]
         });
     }
